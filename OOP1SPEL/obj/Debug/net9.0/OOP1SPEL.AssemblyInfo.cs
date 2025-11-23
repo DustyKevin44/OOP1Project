3ad6585399ef5b8f58315c7e66446150b8ca3994
Kevin <dustykevin44@gmail.com>
2025-11-23 05:48:35 +0100

    Added basic dll

--- OOP1SPEL/obj/Debug/net9.0/OOP1SPEL.AssemblyInfo.cs
+++ OOP1SPEL/obj/Debug/net9.0/OOP1SPEL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP1SPEL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b95eab5b9816201920bc4c85ca84cf8879fdd139")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be07f458210c2bd22a223a705c061f99e3289ad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP1SPEL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP1SPEL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
